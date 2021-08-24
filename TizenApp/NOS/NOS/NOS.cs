e04611bb427f243b376b49f42cd093ad6080e281
ItzLarz <l.y.kruitwagen@gmail.com>
2021-08-24 02:06:59 +0200

    Added Twitter API connection

--- TizenApp/NOS/NOS/NOS.cs
+++ TizenApp/NOS/NOS/NOS.cs
@@ -4,8 +4,9 @@ using System.Collections.Specialized;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Xamarin.Forms;
+using Tweetinvi;
+using Tweetinvi.Models;
 
 namespace NOS
 {
@@ -15,10 +16,16 @@ namespace NOS
         {
             string ConsumerKey = ConfigurationManager.AppSettings.Get("ConsumerKey");
             string ConsumerSecret = ConfigurationManager.AppSettings.Get("ConsumerSecret");
-            string AccesToken = ConfigurationManager.AppSettings.Get("AccessToken");
+            string AccessToken = ConfigurationManager.AppSettings.Get("AccessToken");
             string AccessTokenSecret = ConfigurationManager.AppSettings.Get("AccessTokenSecret");
-        // The root page of your application
-        MainPage = new ContentPage
+
+            var credentials = new TwitterCredentials(ConsumerKey, ConsumerSecret, AccessToken, AccessTokenSecret);
+            var client = new TwitterClient(credentials);
+
+            string user = client.Users.GetUserAsync("pewdiepie").ToString();
+
+            // The root page of your application
+            MainPage = new ContentPage
             {
                 Content = new StackLayout
                 {
@@ -26,7 +33,7 @@ namespace NOS
                     Children = {
                         new Label {
                             HorizontalTextAlignment = TextAlignment.Center,
-                            Text = ConsumerKey
+                            Text = user
                         }
                     }
                 }
