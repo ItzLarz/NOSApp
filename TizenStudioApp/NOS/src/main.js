539f7fcd18eb0b686b73124095f856ba2232ac17
ItzLarz <l.y.kruitwagen@gmail.com>
2023-01-14 20:59:32 +0100

    TAU implemented

--- TizenStudioApp/NOS/src/main.js
+++ TizenStudioApp/NOS/src/main.js
@@ -1,52 +1,46 @@
 // Importing dependencies
-// import { appcommon } from "tizen-tv-webapis";
-// import { application } from "tizen-common-web";
-
+import { appcommon } from "tizen-tv-webapis";
+import { application } from "tizen-common-web";
 
 window.onload = () => {
-	// console.log('onload');
-	// const version = appcommon.getVersion();
-	// console.log('appcommon version : ', version);
-	// document.body.addEventListener('keydown', keydownHandler);
-
-	var box = document.querySelector('#textbox');
-	box.innerHTML = document.domain;
+	const version = appcommon.getVersion();
+	console.log("App version:", version);
+	document.body.addEventListener("keydown", keydownHandler);
 
 	fetch(`https://nos-app.vercel.app/api/rss`)
-		.then(response => response.text())
-		.then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-		.then(doc => {
+		.then((response) => response.text())
+		.then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+		.then((doc) => {
 			let items = doc.querySelectorAll("item");
-			let data = [];
-			items.forEach(item => data.push({ title: item.querySelector("title").textContent, link: item.querySelector('link').textContent, description: item.querySelector('description').textContent, enclosure: item.querySelector('enclosure').getAttribute("url") }))
-			return data
-		})
-		.then(console.log)
+			let titles = [];
+			let imgs = [];
+			let articles = [];
+			let links = [];
+			items.forEach((item) => [
+				titles.push({ title: item.querySelector("title").textContent }),
+				imgs.push({
+					enclosure: item.querySelector("enclosure").getAttribute("url"),
+				}),
+				articles.push({ text: item.querySelector("description").textContent }),
+				links.push({ link: item.querySelector("link").textContent }), // (click to open on phone?)
+			]);
+		});
 
-
-
-
-	var textbox = document.querySelector('.contents');
-	textbox.addEventListener("click", function () {
-		console.log("fakka");
-
-	// 	var data = await scraper.tweetHandler(tweet);
-	// 	for (var i = 0; i < data.length; i++) {
-	// 		console.log(i)
-	// 	}
-
-	});
+	// var textbox = document.querySelector(".contents");
+	// textbox.addEventListener("click", function () {
+	// 	console.log("fakka");
+	// });
 };
 
 // const keyName = {
-// 	10009: 'return'
+// 	10009: "return"
 // };
 
-// function keydownHandler(e) {
-// 	console.log(e.keyCode);
-// 	switch (keyName[e.keyCode]) {
-// 		case 'return':
-// 			application.getCurrentApplication().exit();
-// 			break;
-// 	}
-// }
+function keydownHandler(e) {
+	console.log(e.keyCode);
+	switch (keyName[e.keyCode]) {
+		case "return":
+			application.getCurrentApplication().exit();
+			break;
+	}
+}
