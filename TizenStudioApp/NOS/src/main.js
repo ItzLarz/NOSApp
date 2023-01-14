681387196791c1e7c1427cca41031a913128215e
ItzLarz <l.y.kruitwagen@gmail.com>
2023-01-14 17:04:59 +0100

    .

--- TizenStudioApp/NOS/src/main.js
+++ TizenStudioApp/NOS/src/main.js
@@ -1,69 +1,18 @@
 // Importing dependencies
 // import { appcommon } from "tizen-tv-webapis";
 // import { application } from "tizen-common-web";
-const Twit = require("twit");
-const scraper = require("./webscraper")
 
-// Creating Twitter instance
-// const secret = require("dist/keys.js");
-// const T = new Twit(secret);
-
-function getTweet(id) {
-	return new Promise(function (resolve, reject) {
-		const config = {
-			id: id,
-			tweet_mode: "extended",
-			include_rts: false,
-			exclude_replies: true,
-		};
-
-		T.get("statuses/lookup", config, function (err, data) {
-			if (err != undefined) {
-				console.error(err);
-				reject(err);
-			}
-
-			for (var i = 0; i < data.length; i++) {
-				resolve(data[i].full_text);
-			}
-
-			// scraper.tweetHandler(data[0]);
-		});
-	});
-}
-
-function latestTweets(handle, count) {
-	return new Promise(function (resolve, reject) {
-		const config = {
-			screen_name: handle,
-			count: count,
-			tweet_mode: "extended",
-			include_rts: false,
-			exclude_replies: true,
-		};
-
-		T.get("statuses/user_timeline", config, function (err, data) {
-			if (err != undefined) {
-				console.error(err);
-				reject(err);
-			}
-
-			for (var i = 0; i < data.length; i++) {
-				resolve(data[i]);
-			}
-		});
-	});
-}
 
 window.onload = () => {
 	// console.log('onload');
 	// const version = appcommon.getVersion();
 	// console.log('appcommon version : ', version);
 	// document.body.addEventListener('keydown', keydownHandler);
+
 	var box = document.querySelector('#textbox');
 	box.innerHTML = document.domain;
 
-	fetch(`https://feeds.nos.nl/nosnieuwsalgemeen`,{mode: "no-cors"})
+	fetch(`https://nos-app.vercel.app/api/rss`)
 		.then(response => response.text())
 		.then(str => new window.DOMParser().parseFromString(str, "text/xml"))
 		.then(doc => {
@@ -76,20 +25,17 @@ window.onload = () => {
 
 
 
-	// console.log(title);
 
-	// var textbox = document.querySelector('.contents');
-	// textbox.addEventListener("click", async function () {
-	// 	var tweet = await latestTweets("NOS", 1);
-	// 	var title = tweet.full_text;
-
+	var textbox = document.querySelector('.contents');
+	textbox.addEventListener("click", function () {
+		console.log("fakka");
 
 	// 	var data = await scraper.tweetHandler(tweet);
 	// 	for (var i = 0; i < data.length; i++) {
 	// 		console.log(i)
 	// 	}
 
-	// });
+	});
 };
 
 // const keyName = {
